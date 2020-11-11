e93051d191ea9626094b0872ee053746212cd2a5
Rohan Verma <43259200+rohanvrm@users.noreply.github.com>
2020-11-11 13:40:56 +0530

    search specific headers and get title

--- pyAutoSearch.py
+++ pyAutoSearch.py
@@ -11,12 +11,25 @@ import time
 path=r"C:\Users\Rohan\.wdm\drivers\chromedriver\win32\86.0.4240.22\chromedriver"
 # path of our chrome webdriver
 driver = webdriver.Chrome(path)
+# we named our driver as "driver"
 driver.get("https://www.techwithtim.net/")    
 sleep(5) 
 print(driver.title)
+
 search= driver.find_element_by_name("s")
 search.send_keys("test")
 search.send_keys(Keys.RETURN)   
-time.sleep(5)
-driver.close()
-driver.quit()
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+    #driver.find XX  it's main.find
+    articles=main.find_elements_by_tag_name("article")
+    #articles=main.find_element_by_tag_name("article") this line gives error -> The difference betwn "find_element" and "find_elements" is single item vs list
+    for article in articles:
+        header=article.find_element_by_class_name("entry-summary") 
+        print(header.text)
+        print("")
+finally:
+    #sleep(5)
+    driver.quit()
